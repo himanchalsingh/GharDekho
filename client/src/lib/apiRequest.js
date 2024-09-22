8a913a8de5adaea0ba58b8a0772b743accee69b0
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2024-09-23 01:20:26 +0530

    save deploy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://ghardekho-pslm.onrender.com/",
   withCredentials: true,
 });
 
