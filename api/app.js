9cdd75e29e7e5cacd9aa7ca51013e072b6bcd0cc
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2024-09-25 18:20:51 +0530

    add

--- api/app.js
+++ api/app.js
@@ -7,17 +7,31 @@ import userRouter from "./routes/user.route.js";
 import messageRouter from "./routes/message.route.js";
 import chatRouter from "./routes/chat.route.js";
 import cors from "cors";
+
 const app = express();
-app.use(cors());
+
+// CORS configuration
+const corsOptions = {
+  origin: "https://ghar-dekho-jet.vercel.app", // Replace with your client domain
+  credentials: true, // Allow credentials such as cookies or authorization headers
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions));
+
 app.use(cookieParser());
 app.use(express.json());
+
+// Route handlers
 app.use("/api/post", postRouter);
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/test", testRouter);
 app.use("/api/chats", chatRouter);
 app.use("/api/messages", messageRouter);
+
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`Server is running on port ${PORT}`);
 });
