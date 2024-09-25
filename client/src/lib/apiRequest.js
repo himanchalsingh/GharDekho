a74fe8d3a854e056357671c4f24c8fd5b0e4995d
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2024-09-25 19:02:53 +0530

    add script

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://ghardekho-egog.onrender.com/api",
+  baseURL: "http://localhost:8800/api",
   withCredentials: true,
 });
 
